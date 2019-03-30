5eacd4a4ef86d0acbd5b07034a8ef98c16f5e97b
itcast <bos@itcast.cn>
2019-03-31 00:47:53 +0800

    方法

--- Day_01/src/test/java/com/test/Demo01.java
+++ Day_01/src/test/java/com/test/Demo01.java
@@ -1,11 +1,5 @@
 package com.test;
 
 public class Demo01 {
-    public static void main(String[] args) {
-        System.out.println("Day02");
-        System.out.println("2的");
-    }
-    public static void main(String[] args) {
-        System.out.println("Day01");
-    }
+
 }
