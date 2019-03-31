9e57ddb0b3727878c7e099ce099070de2fce7192
itcast <bos@itcast.cn>
2019-03-31 14:41:28 +0800

    新建11

--- Day_02/src/test/java/Demo01.java
+++ Day_02/src/test/java/Demo01.java
@@ -2,5 +2,6 @@ public class Demo01 {
     public static void main(String[] args) {
         System.out.println("Demo01");
         System.out.println("Demo11");
+        System.out.println("Demo22");
     }
 }
